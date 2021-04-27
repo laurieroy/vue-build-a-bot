4c6e59df68a03981d9782aa6b8c6624c3d7fb616
lauretteroy <laurieroy.dev@gmail.com>
2021-04-27 10:38:07 -0600

    Add HomePage, Build

---
+++ src/home/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <img
+      class="rotbot"
+      src="../assets/robot-home.png"
+      alt="robot"
+      aria-hidden="true"
+    />
+    <div class="get-started">
+      <a href="">Get Started</a> building your first robot!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .home {
+    text-align: center;
+  }
+  .robot {
+    height: 300px;
+  }
+</style>
